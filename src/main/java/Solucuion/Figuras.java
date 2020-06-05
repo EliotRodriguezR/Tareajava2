7a7849069075436b8fc5e5e8777025931fb7de58
erodriguez19Uni <eliot.sereck@gmail.com>
2020-06-05 14:33:23 -0500

    Tarea2

---
+++ src/main/java/Solucuion/Figuras.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Solucuion;
+
+/**
+ *
+ * @author Ivan
+ */
+
+public class Figuras {
+        float base;
+        float altura;
+        float r;
+        
+public float calcularArea (){
+            return 0;
+    
+
+         
+    }
+  
+}
---
+++ src/main/java/Solucuion/Triangulo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Solucuion;
+
+/**
+ *
+ * @author Ivan
+ */
+public class Triangulo extends Figuras{
+    
+    public float calcularArea(){
+     float resultado=base*altura/2;
+     
+     return resultado;
+        
+        
+    }
+    }
+
