d81e1fc75899d5313c887e7a3cfaee5ad201d922
erodriguez19Uni <eliot.sereck@gmail.com>
2020-06-05 14:29:21 -0500

    Tarea2

---
+++ src/main/java/Solucuion/Circulos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Solucuion;
+/**
+ *
+ * @author Ivan
+ */
+public class Circulos  extends Figuras{
+    
+    public float calcularArea(){
+        
+     float resultado= (float) (Math.PI * Math.pow(r,2));
+     
+     return resultado;
+        
+        
+    }
+    
+}
